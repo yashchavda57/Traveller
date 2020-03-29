ce6fd7dadc76920b0c53e0ddd76c1d393e47fabe
yashchavda57 <chavdayash1709@gmail.com>
2020-03-29 17:14:32 +0530

    My First Commit

---
+++ travello/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Destination(models.Model):
+    
+    name = models.CharField(max_length = 100)
+    desc = models.TextField()
+    price = models.IntegerField()
+    img = models.ImageField(upload_to = 'pics')
+    offers = models.BooleanField(default = False)
+    
+
